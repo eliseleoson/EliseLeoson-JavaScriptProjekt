fa4bf53458ae082216b6abf6cb81f372b1f15524
eliseleoson <leoson.elise@elev.ga.ntig.se>
2022-03-11 16:21:45 +0100

    first commit

---
+++ Quiz.js
@@ -0,0 +1,31 @@
+let form = document.querySelector("form");
+console.log(form);
+let resultBox = document.querySelector(".result-box");
+
+form.addEventListener("submit", handleForm);
+
+let points = 0;
+
+function handleForm(event) {
+    event.preventDefault();
+    let answers = Array.from(document.querySelector("[type='radio']"));
+    console.log(answers);
+    answers.forEach((answer) => {
+        if (answer.checked) {
+            points += parseInt(answer.value);
+        }
+    })
+    console.log(points);
+
+    if (points === 3) {
+        window.location.href = "./result1.html";
+    } else if (points >= 666 && points <= 668) {
+        window.location.href = "https://www.komvuxutbildningar.se/utbildning/komvuxutbildning-matematik";
+    } else if (points >= 2000 && points <= 2003) {
+        window.location.href = "./knug.html";
+    } else if (points === 1 && points === 2 ) {
+        window.location.href = "./result2.html";
+    } else if (points === 0) {
+        window.location.href = "./result3.html";
+    }
+}
